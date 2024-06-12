ae3674158c392da4ce1ec22d8e49103672f12287
LawiK974 <loic97429@gmail.com>
2024-06-12 10:29:10 +0200

    Formatting mattermost messages and update deployement manifest

--- kube_notify/mattermost.py
+++ kube_notify/mattermost.py
@@ -16,15 +16,16 @@ def send_mattermost_message(
 ) -> None:
     # Construct the HTTP request for sending a message to Mattermost
     headers = {"Content-Type": "application/json"}
-    message = f"### {title}\n\n"
-    message += f"**Description : {description}**\n\n"
+    message = f"#### {title} : {description}\n"
 
     for key, value in fields.items():
-        message += f"*{key} :*\n{value}\n\n"
+        message += f"**{key} :** {value}\n"
     data = {"text": message}
     channel and data.update({"channel": channel})
     username and data.update({"username": username})
     icon_url and data.update({"icon_url": icon_url})
     response = requests.post(webhook, headers=headers, data=json.dumps(data))
     if response.status_code != 200:
-        logger.logger.error("Failed to send notification to Mattermost")
+        logger.logger.error(
+            f"[{response.status_code}] Failed to send notification to Mattermost : {str(response.content)}"
+        )
